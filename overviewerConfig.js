78f0eb91348dd79d3361fdca4fb69ea174902835
HoangTheBoss <hoangtheboss.hyper@gmail.com>
2022-06-12 11:13:12 +0700

    init

---
+++ overviewerConfig.js
@@ -0,0 +1,206 @@
+var overviewerConfig = {
+    "CONST": {
+        "tileSize": 384,
+        "image": {
+            "defaultMarker": "signpost.png",
+            "signMarker": "signpost_icon.png",
+            "bedMarker": "bed.png",
+            "spawnMarker": "markers/marker_home.png",
+            "spawnMarker2x": "markers/marker_home_2x.png",
+            "queryMarker": "markers/marker_location.png",
+            "queryMarker2x": "markers/marker_location_2x.png",
+            "compass": {
+                "0": "compass_upper-left.png",
+                "1": "compass_upper-right.png",
+                "3": "compass_lower-left.png",
+                "2": "compass_lower-right.png"
+            }
+        },
+        "mapDivId": "mcmap",
+        "UPPERLEFT": 0,
+        "UPPERRIGHT": 1,
+        "LOWERLEFT": 3,
+        "LOWERRIGHT": 2
+    },
+    "worlds": [
+        "simp - overworld"
+    ],
+    "map": {
+        "debug": false,
+        "cacheTag": "1655006360",
+        "north_direction": "lower-left",
+        "controls": {
+            "pan": true,
+            "zoom": true,
+            "spawn": true,
+            "compass": true,
+            "mapType": true,
+            "overlays": true,
+            "coordsBox": true
+        }
+    },
+    "tilesets": [
+        {
+            "name": "Overwold Daytime",
+            "zoomLevels": 7,
+            "defaultZoom": 1,
+            "maxZoom": 7,
+            "path": "overworld_day",
+            "base": "",
+            "bgcolor": "#1a1a1a",
+            "world": "simp - overworld",
+            "last_rendertime": 1655003398,
+            "imgextension": "png",
+            "isOverlay": false,
+            "poititle": "Markers",
+            "showlocationmarker": true,
+            "center": [
+                -50,
+                64,
+                -990
+            ],
+            "minZoom": 0,
+            "spawn": [
+                56,
+                161,
+                -70
+            ],
+            "north_direction": 2
+        },
+        {
+            "name": "Overwold Daytime Flipped",
+            "zoomLevels": 7,
+            "defaultZoom": 1,
+            "maxZoom": 7,
+            "path": "overworld_day_flipped",
+            "base": "",
+            "bgcolor": "#1a1a1a",
+            "world": "simp - overworld",
+            "last_rendertime": 1655003398,
+            "imgextension": "png",
+            "isOverlay": false,
+            "poititle": "Markers",
+            "showlocationmarker": true,
+            "center": [
+                -50,
+                64,
+                -990
+            ],
+            "minZoom": 0,
+            "spawn": [
+                56,
+                161,
+                -70
+            ],
+            "north_direction": 0
+        },
+        {
+            "name": "Overwold Nighttime",
+            "zoomLevels": 7,
+            "defaultZoom": 1,
+            "maxZoom": 7,
+            "path": "overworld_night",
+            "base": "",
+            "bgcolor": "#1a1a1a",
+            "world": "simp - overworld",
+            "last_rendertime": 1655003398,
+            "imgextension": "png",
+            "isOverlay": false,
+            "poititle": "Markers",
+            "showlocationmarker": true,
+            "center": [
+                -50,
+                64,
+                -990
+            ],
+            "minZoom": 0,
+            "spawn": [
+                56,
+                161,
+                -70
+            ],
+            "north_direction": 2
+        },
+        {
+            "name": "Overwold Nighttime Flipped",
+            "zoomLevels": 7,
+            "defaultZoom": 1,
+            "maxZoom": 7,
+            "path": "overworld_night_flipped",
+            "base": "",
+            "bgcolor": "#1a1a1a",
+            "world": "simp - overworld",
+            "last_rendertime": 1655003398,
+            "imgextension": "png",
+            "isOverlay": false,
+            "poititle": "Markers",
+            "showlocationmarker": true,
+            "center": [
+                -50,
+                64,
+                -990
+            ],
+            "minZoom": 0,
+            "spawn": [
+                56,
+                161,
+                -70
+            ],
+            "north_direction": 0
+        },
+        {
+            "name": "Overwold Caves",
+            "zoomLevels": 7,
+            "defaultZoom": 1,
+            "maxZoom": 7,
+            "path": "overworld_caves",
+            "base": "",
+            "bgcolor": "#1a1a1a",
+            "world": "simp - overworld",
+            "last_rendertime": 1654997611,
+            "imgextension": "png",
+            "isOverlay": false,
+            "poititle": "Markers",
+            "showlocationmarker": true,
+            "center": [
+                -50,
+                64,
+                -990
+            ],
+            "minZoom": 0,
+            "spawn": [
+                56,
+                161,
+                -70
+            ],
+            "north_direction": 2
+        },
+        {
+            "name": "Overwold Caves Flipped",
+            "zoomLevels": 7,
+            "defaultZoom": 1,
+            "maxZoom": 7,
+            "path": "overworld_caves_flipped",
+            "base": "",
+            "bgcolor": "#1a1a1a",
+            "world": "simp - overworld",
+            "last_rendertime": 1654997611,
+            "imgextension": "png",
+            "isOverlay": false,
+            "poititle": "Markers",
+            "showlocationmarker": true,
+            "center": [
+                -50,
+                64,
+                -990
+            ],
+            "minZoom": 0,
+            "spawn": [
+                56,
+                161,
+                -70
+            ],
+            "north_direction": 0
+        }
+    ]
+};
